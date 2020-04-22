cb995c70667d57cb9b93b7ebdc5d585385f82a6e
hymanHu <hujiangyx@163.com>
2020-04-22 10:24:01 +0800

    "Create spring boot app"

---
+++ springBootDemo2001_1/src/main/java/com/thornBird/sbd/modules/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.thornBird.sbd.modules.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/api/test/desc")
+	@ResponseBody
+	public String getAppDesc() {
+		return "This is spring boot app.";
+	}
+}
