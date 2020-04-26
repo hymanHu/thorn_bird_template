677a945112950a88dd5334a579e3e7f0b8c213ac
hymanHu <hujiangyx@163.com>
2020-04-26 10:41:35 +0800

    "fragments"

--- springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestPageController.java
+++ springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestPageController.java
@@ -8,6 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/test")
 public class TestPageController {
 	
+	@RequestMapping("/index1")
+	public String testIndexPage() {
+		return "test/index1";
+	}
+	
+	@RequestMapping("/index2")
+	public String testIndexPage2() {
+		return "test/index2";
+	}
+	
+	@RequestMapping("/index3")
+	public String testIndexPage3() {
+		return "test/index3";
+	}
+	
 	@RequestMapping("/index")
 	public String testIndexPage(ModelMap modelMap) {
 		modelMap.addAttribute("template", "test/index");
