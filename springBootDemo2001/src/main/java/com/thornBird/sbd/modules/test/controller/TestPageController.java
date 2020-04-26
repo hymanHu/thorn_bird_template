0f4a67e307a6c2c74890d186cf38b825f479d587
hymanHu <hujiangyx@163.com>
2020-04-26 11:43:09 +0800

    "Thymleaf 02"

--- springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestPageController.java
+++ springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestPageController.java
@@ -1,12 +1,49 @@
 package com.thornBird.sbd.modules.test.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.thornBird.sbd.modules.test.entity.City;
+import com.thornBird.sbd.modules.test.entity.Country;
+import com.thornBird.sbd.modules.test.service.CityService;
+import com.thornBird.sbd.modules.test.service.CountryService;
+
 @Controller
 @RequestMapping("/test")
 public class TestPageController {
+	
+	@Autowired
+	private CountryService countryService;
+	@Autowired
+	private CityService cityService;
+	
+	@RequestMapping("/index")
+	public String testIndexPage(ModelMap modelMap) {
+		int countryId = 522;
+		Country country = countryService.getCountryById(countryId);
+		List<City> cities = cityService.getCitiesByCountryId(countryId);
+		cities = cities.stream().limit(10).collect(Collectors.toList());
+		City city = cities.get(0);
+		
+		modelMap.addAttribute("thymeleafTitle", "thymeleaf Title");
+		modelMap.addAttribute("checked", true);
+		modelMap.addAttribute("currentNumber", 99);
+		modelMap.addAttribute("changeType", "checkbox");
+		modelMap.addAttribute("baiduUrl", "http://www.baidu.com");
+		modelMap.addAttribute("shopLogo", "http://cdn.duitang.com/uploads"
+				+ "/item/201308/13/20130813115619_EJCWm.thumb.700_0.jpeg");
+		modelMap.addAttribute("country", country);
+		modelMap.addAttribute("city", city);
+		modelMap.addAttribute("updateCityUri", "/api/city");
+		modelMap.addAttribute("cities", cities);
+		modelMap.addAttribute("template", "test/index");
+		return "index";
+	}
 	
 	@RequestMapping("/index1")
 	public String testIndexPage() {
@@ -21,11 +58,5 @@ public class TestPageController {
 	@RequestMapping("/index3")
 	public String testIndexPage3() {
 		return "test/index3";
-	}
-	
-	@RequestMapping("/index")
-	public String testIndexPage(ModelMap modelMap) {
-		modelMap.addAttribute("template", "test/index");
-		return "index";
 	}
 }
