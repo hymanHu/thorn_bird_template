83dfebc7e9addc7c467dd7c30e38d3d858211553
hymanHu <hujiangyx@163.com>
2020-04-27 12:03:07 +0800

    "Upload file and downLoad file"

--- springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestController.java
+++ springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestController.java
@@ -1,11 +1,24 @@
 package com.thornBird.sbd.modules.test.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Paths;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +44,95 @@ public class TestController {
 	
 	@Autowired
 	private ConfigBean configBean;
+	
+	@RequestMapping("/download")
+	public ResponseEntity<Resource> downLoadFile(@RequestParam String fileName) {
+		try {
+			Resource resource = new UrlResource(Paths.get("D:\\upload\\" + fileName).toUri());
+			
+			return ResponseEntity.ok()
+					.header(HttpHeaders.CONTENT_TYPE, "application/octet-stream")
+					.header(HttpHeaders.CONTENT_DISPOSITION, 
+							String.format("attachment; filename=\"%s\"", resource.getFilename()))
+					.body(resource);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * 将文件以BufferedInputStream的方式读取到byte[]里面，然后用OutputStream.write输出文件
+	 */
+	@RequestMapping("/download1")
+	public void downloadFile1(HttpServletRequest request, 
+			HttpServletResponse response, @RequestParam String fileName) {
+		String filePath = "D:/upload" + File.separator + fileName;
+		File downloadFile = new File(filePath);
+		
+		if (downloadFile.exists()) {
+			response.setContentType("application/octet-stream");
+			response.setContentLength((int)downloadFile.length());
+			response.setHeader(HttpHeaders.CONTENT_DISPOSITION, 
+					String.format("attachment; filename=\"%s\"", fileName));
+			
+			byte[] buffer = new byte[1024];
+			FileInputStream fis = null;
+			BufferedInputStream bis = null;
+			try {
+				fis = new FileInputStream(downloadFile);
+				bis = new BufferedInputStream(fis);
+				OutputStream os = response.getOutputStream();
+				int i = bis.read(buffer);
+				while (i != -1) {
+					os.write(buffer, 0, i);
+					i = bis.read(buffer);
+				}
+			} catch (Exception e) {
+				LOGGER.debug(e.getMessage());
+				e.printStackTrace();
+			} finally {
+				try {
+					if (fis != null) {
+						fis.close();
+					}
+					if (bis != null) {
+						bis.close();
+					}
+				} catch (Exception e2) {
+					LOGGER.debug(e2.getMessage());
+					e2.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 以包装类 IOUtils 输出文件
+	 */
+	@RequestMapping("/download2")
+	public void downloadFile2(HttpServletRequest request, 
+			HttpServletResponse response, @RequestParam String fileName) {
+		String filePath = "D:/upload" + File.separator + fileName;
+		File downloadFile = new File(filePath);
+		
+		try {
+			if (downloadFile.exists()) {
+				response.setContentType("application/octet-stream");
+				response.setContentLength((int)downloadFile.length());
+				response.setHeader(HttpHeaders.CONTENT_DISPOSITION, 
+						String.format("attachment; filename=\"%s\"", fileName));
+				
+				InputStream is = new FileInputStream(downloadFile);
+				IOUtils.copy(is, response.getOutputStream());
+				response.flushBuffer();
+			}
+		} catch (Exception e) {
+			LOGGER.debug(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 	
 	/**
 	 * http://127.0.0.1/api/test/log
--- springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestPageController.java
+++ springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestPageController.java
@@ -1,12 +1,18 @@
 package com.thornBird.sbd.modules.test.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.thornBird.sbd.modules.test.entity.City;
 import com.thornBird.sbd.modules.test.entity.Country;
@@ -21,6 +27,75 @@ public class TestPageController {
 	private CountryService countryService;
 	@Autowired
 	private CityService cityService;
+	
+	/**
+	 * 127.0.0.1/test/files
+	 */
+	@PostMapping(value="/files", consumes="multipart/form-data")
+	public String uploadFiles(@RequestParam MultipartFile[] files, 
+			RedirectAttributes redirectAttributes) {
+		boolean isEmpty = true;
+		
+		try {
+			for (MultipartFile file : files) {
+				if (file.isEmpty()) {
+//				break;
+					continue;
+				}
+				
+				String fileName = file.getOriginalFilename();
+				String destFilePath = "D:\\upload\\" + fileName;
+				File destFile = new File(destFilePath);
+				file.transferTo(destFile);
+				
+				isEmpty = false;
+			}
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("message", "Upload file failed.");
+			return "redirect:/test/index";
+		}
+		
+		if (isEmpty) {
+			redirectAttributes.addFlashAttribute("message", "Please select file.");
+		} else {
+			redirectAttributes.addFlashAttribute("message", "Upload file success.");
+		}
+		return "redirect:/test/index";
+	}
+	
+	/**
+	 * 127.0.0.1/test/file
+	 */
+	@PostMapping(value="/file", consumes = "multipart/form-data")
+	public String uploadFile(@RequestParam MultipartFile file, 
+			RedirectAttributes redirectAttributes) {
+		
+		if (file.isEmpty()) {
+			redirectAttributes.addFlashAttribute("message", "Please select file.");
+			return "redirect:/test/index";
+		}
+		
+		try {
+			String fileName = file.getOriginalFilename();
+			String destFilePath = "D:\\upload\\" + fileName;
+			File destFile = new File(destFilePath);
+			file.transferTo(destFile);
+			
+			// 使用工具类Files来上传文件
+//			byte[] bytes = file.getBytes();
+//			Path path = Paths.get(destFileName);
+//			Files.write(path, bytes);
+			
+			redirectAttributes.addFlashAttribute("message", "Upload file success.");
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("message", "Upload file failed.");
+			return "redirect:/test/index";
+		}
+		
+		return "redirect:/test/index";
+	}
 	
 	/**
 	 * 127.0.0.1/test/index
