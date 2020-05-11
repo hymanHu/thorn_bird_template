65bc3f3af5f9eec77759a2c53df90c195cd22442
hymanHu <hujiangyx@163.com>
2020-05-11 19:25:23 +0800

    "test"

--- springBootDemo2001/src/main/java/com/thornBird/sbd/SpringBootDemo2001Application.java
+++ springBootDemo2001/src/main/java/com/thornBird/sbd/SpringBootDemo2001Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemo2001Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemo2001Application.class, args);
 	}
